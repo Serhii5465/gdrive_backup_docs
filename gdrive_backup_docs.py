f27237b1d8b81f4767149a4c7b1b270a28781c65
Serhii5465 <serhiiartiukh5@gmail.com>
2024-01-04 13:32:24 +0200

    Added paths validity check

--- gdrive_backup_docs.py
+++ gdrive_backup_docs.py
@@ -1,28 +1,20 @@
-import os
 import sys
 import argparse
 import datetime
+import os
 from pathlib import Path
 from py_exec_cmd import exec_cmd
-from typing import List, Dict
+from typing import Dict
+import rclone_const
 
 
-def check_files(*list_files: List[str]) -> None:
-    """
-    Verifies if the path to a folder or file is valid. If at least one path is broken,
-    the script terminates.
-    Args:
-        *list_files: List of full paths to folders and files.
-    """
-    for i in list_files:
-        if not os.path.exists(i):
-            msg = i + " doesn't exists"
-            sys.exit(msg)
+def create_dir(dir: str) -> None:
+    Path(dir).mkdir(parents=True, exist_ok=True)
 
 
 def parse_args() -> Dict[str, bool]:
     """
-    Handles command-line options to select the script's modeю.
+    Handles command-line options to select the script's mode.
     Returns:
         Dictionary with active mode
     """
@@ -40,50 +32,60 @@ def parse_args() -> Dict[str, bool]:
     return args
 
 
-def upload_to_gdrive(sync_dir: str, logs_dir: str) -> None:
+def upload_to_gdrive() -> None:
     """
-    Uploads files from local folder to Google Drive.
+    Checking all paths for validity and uploading files from local folder to Google Drive.
     Under normal conditions the process finishes work and has an exit code of 0.
     Otherwise, the process has an exit code different from 0 and the script terminates.
-    Args:
-        sync_dir: Path to the synchronized folder.
-        logs_dir: Path to the logs folder.
     """
     args = parse_args()
     
     #: Converting Unix-like path to Windows form by using Cygpath.exe utility.
-    win_style_path_sync_dir = exec_cmd.get_cmd_out(['cygpath', '--windows', sync_dir])
-    win_style_path_logs_dir = exec_cmd.get_cmd_out(['cygpath', '--windows', logs_dir])
+    win_style_path_sync_dir = exec_cmd.get_cmd_out(['cygpath', '--windows', rclone_const.DOCS_DIR()])
+    win_style_path_logs_dir = exec_cmd.get_cmd_out(['cygpath', '--windows', rclone_const.LOGS_DIR()])
 
     command = ['rclone', 'sync', '--progress', '--verbose',
-                '--log-file=' + win_style_path_logs_dir.stdout.strip('\n') + '/' + datetime.datetime.now().strftime("%Y-%m-%d_%H\uA789%M\uA789%S") + '.log']
-
+                '--log-file=' + win_style_path_logs_dir.stdout.strip('\n') + datetime.datetime.now().strftime("%Y-%m-%d_%H\uA789%M\uA789%S") + '.log']
+    
     if args['upload']:
-        command.insert(4, win_style_path_sync_dir.stdout.strip('\n'))
-        command.insert(5, 'google-drive:')
+        #: Checking for the presence of the local root folder with documents
+        try:
+            dirs = os.listdir(rclone_const.DOCS_DIR())
+        except FileNotFoundError:
+            sys.exit('Directory ' + rclone_const.DOCS_DIR() + ' is missing')
+        else:
+            if len(dirs) == 0:
+                raise IndexError('Directory ' + rclone_const.DOCS_DIR() + ' is empty')
 
-    elif args['download']:
-        command.insert(4, 'google-drive:')
+        command.insert(4, win_style_path_sync_dir.stdout.strip('\n'))
+        command.insert(5, rclone_const.ROOT_REMOTE_DIR())
+
+    if args['download']:
+        #: Checking for the presence of the remote root folder with documents on Google Drive
+        is_rem_dir_empty = exec_cmd.get_cmd_out(['rclone', 'lsf', rclone_const.ROOT_REMOTE_DIR()])
+        if not is_rem_dir_empty.stdout:
+            raise FileNotFoundError('Source directory on Google Drive is empty')
+
+        if not os.path.exists(rclone_const.DOCS_DIR()):
+            create_dir(rclone_const.DOCS_DIR())
+            print('Creating directory ' + rclone_const.DOCS_DIR())
+
+        command.insert(4, rclone_const.ROOT_REMOTE_DIR())
         command.insert(5, win_style_path_sync_dir.stdout.strip('\n'))
 
-    out = exec_cmd.run_cmd(command)
+    create_dir(rclone_const.LOGS_DIR())
 
+    out = exec_cmd.run_cmd(command)
     if out.returncode != 0:
-        sys.exit('Error\nCheck logs')
+        raise RuntimeError('Error\nCheck logs')
     else:
-        print('\nGood')
+        print('\nSuccess')
 
 
 def main() -> None:
-    rclone_conf = str(Path.home()) + '/.config/rclone/rclone.conf'
-    sync_dir = '/cygdrive/e/documents'
+    if not os.path.exists(rclone_const.RCLONE_CONF_FILE()):
+        raise FileNotFoundError('Rclone\'s config file is missing')
 
-    check_files(rclone_conf, sync_dir)
-
-    logs_dir = '/cygdrive/e/logs/rclone/'
-    Path(logs_dir).mkdir(parents=True, exist_ok=True)
-
-    upload_to_gdrive(sync_dir, logs_dir)
-
+    upload_to_gdrive()
 
 main()
