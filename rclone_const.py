ae0d6165c5715c2aa7eabbfff5ab1a37843271ec
Serhii5465 <serhiiartiukh5@gmail.com>
2024-02-22 01:00:49 +0200

    Updated

--- rclone_const.py
+++ rclone_const.py
@@ -4,10 +4,10 @@ def RCLONE_CONF_FILE() -> str:
     return str(Path.home()) + '/.config/rclone/rclone.conf'
 
 def DOCS_DIR() -> str:
-    return '/cygdrive/e/documents'
+    return '/cygdrive/d/documents'
 
 def ROOT_REMOTE_DIR() -> str:
     return 'google-drive:'
 
 def LOGS_DIR() -> str:
-    return '/cygdrive/e/logs/rclone/'
+    return '/cygdrive/d/logs/rclone/'
