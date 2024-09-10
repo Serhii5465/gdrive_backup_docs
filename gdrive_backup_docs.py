c90f87aecdfd386cede80cc5e672bf2539bddb47
Serhii5465 <serhiiartiukh5@gmail.com>
2024-09-10 22:02:50 +0300

    gdrive_backup_docs - update

--- gdrive_backup_docs.py
+++ gdrive_backup_docs.py
@@ -47,30 +47,30 @@ def upload_to_gdrive() -> None:
     
     log_file = os.path.join(constants.RCLONE_LOGS_DIR, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.log')
 
-    command = ['rclone', 'sync', '--dry-run', '--progress', '--verbose', '--config=' + constants.RCLONE_CONF_FILE, '--log-file=' + log_file]
+    command = ['rclone', 'sync', '--dry-run', '--progress', '--verbose', f'--config={constants.RCLONE_CONF_FILE}', f'--log-file={log_file}']
     
     if args['upload']:
         #: Checking for the presence of the local root folder with documents
         try:
             dirs = os.listdir(constants.PATH_SYNC_DIR)
         except FileNotFoundError:
-            sys.exit('Directory ' + constants.PATH_SYNC_DIR + ' is missing')
+            sys.exit(f'Directory {constants.PATH_SYNC_DIR} is missing')
         else:
             if len(dirs) == 0:
-                raise IndexError('Directory ' + constants.PATH_SYNC_DIR + ' is empty')
+                raise IndexError(f'Directory {constants.PATH_SYNC_DIR} is empty')
 
         command.insert(4, constants.PATH_SYNC_DIR)
         command.insert(5, constants.ROOT_REMOTE_DIR)
 
     if args['download']:
         #: Checking for the presence of the remote root folder with documents on Google Drive
-        is_rem_dir_empty = subprocess.run(['rclone', '--config=' + constants.RCLONE_CONF_FILE, 'lsf', constants.ROOT_REMOTE_DIR], capture_output=True, text=True)
+        is_rem_dir_empty = subprocess.run(['rclone', f'--config={constants.RCLONE_CONF_FILE}', 'lsf', constants.ROOT_REMOTE_DIR], capture_output=True, text=True)
         if not is_rem_dir_empty.stdout:
             raise FileNotFoundError('Source directory on Google Drive is empty')
 
         if not os.path.exists(constants.PATH_SYNC_DIR):
             create_dir(constants.PATH_SYNC_DIR)
-            print('Creating directory ' + constants.PATH_SYNC_DIR)
+            print(f'Creating directory {constants.PATH_SYNC_DIR}')
 
         command.insert(4, constants.ROOT_REMOTE_DIR)
         command.insert(5, constants.PATH_SYNC_DIR)
