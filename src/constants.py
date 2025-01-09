a1c8ac950f21fbf73beef9ffad455b94397a216f
Serhii5465 <serhiiartiukh5@gmail.com>
2025-01-09 23:19:15 +0200

    Updating the configuration and UUID of the source disk

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,7 @@
 from os import path
 from src import mnt
 
-UUID_DELL_HDD = '5837B806'
-
-UUID_MSI_HDD = '7E0E4F54'
+UUID_SRC_DRIVE = '00A559C0'
 
 ROOT_REMOTE_DIR = 'gdrive:'
 
@@ -11,7 +9,7 @@ NAME_SYNC_DIR = 'gdrive_share'
 
 MNT_POINT_SRC = mnt.get_mnt_point()
 
-RCLONE_CONF_FILE = path.join(MNT_POINT_SRC, 'local', 'rclone', 'rclone.conf')
+RCLONE_CONF_FILE = path.join(MNT_POINT_SRC, 'system', 'local', 'rclone', 'rclone.conf')
 
 RCLONE_LOGS_DIR = path.join(MNT_POINT_SRC, 'logs', 'rclone_gdrive_documents')
 
--- src/mnt.py
+++ src/mnt.py
@@ -2,18 +2,12 @@ import wmi
 from src import constants
 
 def get_mnt_point():
-    uuid_src = [
-        constants.UUID_DELL_HDD,
-        constants.UUID_MSI_HDD
-    ]
-
     try:
         c = wmi.WMI()
         
         for i in c.Win32_LogicalDisk():
-            for j in uuid_src:
-                if i.VolumeSerialNumber and i.VolumeSerialNumber.strip() == j:
-                    return i.DeviceID + '\\'
+            if i.VolumeSerialNumber and i.VolumeSerialNumber.strip() == constants.UUID_SRC_DRIVE:
+                return i.DeviceID + '\\'
         
         print("UUID not found")
         return None
