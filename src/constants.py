80c365df5bb7844f2f80ce4faa0f7ee8b23efbb5
Serhii5465 <serhiiartiukh5@gmail.com>
2025-01-21 15:56:08 +0200

    constants.py: Relocating logs to the user's home directory

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
-from os import path
+import os
 from src import mnt
 
 UUID_SRC_DRIVE = '00A559C0'
@@ -9,8 +9,8 @@ NAME_SYNC_DIR = 'gdrive_share'
 
 MNT_POINT_SRC = mnt.get_mnt_point()
 
-RCLONE_CONF_FILE = path.join(MNT_POINT_SRC, 'system', 'local', 'rclone', 'rclone.conf')
+RCLONE_CONF_FILE = os.path.join(MNT_POINT_SRC, 'system', 'local', 'rclone', 'rclone.conf')
 
-RCLONE_LOGS_DIR = path.join(MNT_POINT_SRC, 'logs', 'rclone_gdrive_documents')
+RCLONE_LOGS_DIR = os.path.join(os.environ["HOME"], 'logs', 'rclone_gdrive_documents')
 
-PATH_SYNC_DIR = path.join(MNT_POINT_SRC, NAME_SYNC_DIR)
+PATH_SYNC_DIR = os.path.join(MNT_POINT_SRC, NAME_SYNC_DIR)
